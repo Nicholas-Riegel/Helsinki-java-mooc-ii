721da740d6235376b0f2ae0aa4cb859fdbe4427b
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-03-28 20:21:45 +0100

    .

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,35 @@
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int total = 0;
 
+        while (true) {
+
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0){
+                break;
+            }
+
+            if (input > 0){
+                numbers.add(input);
+            }
+        }
+
+        for (int num : numbers){
+            total += num;
+        }
+
+        if (numbers.size() == 0){
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((double) total / numbers.size());
+        }
     }
 }
