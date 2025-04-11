95df373da4f5a59b84d9a33dc216aa9c28bf058b
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-04-11 11:50:08 +0200

    part 9 finished

---
+++ part09-Part09_12.Animals/src/main/java/Cat.java
@@ -0,0 +1,20 @@
+// Next to implement is the class Cat, that also inherits from the Animal class. Cat should have two constructors: one with a parameter, used to name the cat according to the parameter, and one without parameters, in which case the name is simply "Cat". Another method for Cat is a non-parameterized method called purr that returns no value (void). Cats should be able to eat and sleep like in the first part.
+
+public class Cat extends Animal implements NoiseCapable{
+
+    public Cat(String name){
+        super(name);
+    }
+
+    public Cat(){
+        super("Cat");
+    }
+
+    public void purr(){
+        System.out.println(getName() + " purrs");
+    }
+
+    public void makeNoise(){
+        purr();
+    }
+}
---
+++ part09-Part09_12.Animals/src/main/java/Dog.java
@@ -0,0 +1,20 @@
+// Implement a class called Dog that inherits from Animal. Dog should have a parameterized constructor that can be used to name it. The class should also have a non-parameterized constructor, which gives the dog the name "Dog". Another method that Dog must have is the non-parameterized bark, and it should not return any value (void). Like all animals, Dog needs to have the methods eat and sleep.
+
+public class Dog extends Animal implements NoiseCapable{
+
+    public Dog(String name){
+        super(name);
+    }
+
+    public Dog(){
+        super("Dog");
+    }
+
+    public void bark(){
+        System.out.println(getName() + " barks");
+    }
+
+    public void makeNoise(){
+        bark();
+    }
+}
