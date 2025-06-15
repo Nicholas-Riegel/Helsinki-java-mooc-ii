926ed088f4449e57dc54023997270eddd0241ebf
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-06-15 21:50:07 +0200

    .

--- part12-Part12_02.Pipe/src/main/java/Pipe.java
+++ part12-Part12_02.Pipe/src/main/java/Pipe.java
@@ -31,5 +31,4 @@ public class Pipe<T> {
         }
         return true;
     } 
-
 }
--- part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -1,13 +1,15 @@
-
-import java.util.Scanner;
-
 public class Program {
     
     // The method must calculate the sum of the elements in the array between the lower and the upper limits. Only numbers smaller or equal to the int largest and larger or equal to the int smallest are added to the sum.
 
     // The method must also check that the lower and the upper limit are valid indexes in the array. If the parameter fromWhere is smaller than 0, the lower limit becomes 0 instead. Accordingly, if the parameter toWhere is larger than the size of the array, the upper limit becomes the last index of the array instead.
     
-    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest){
+    public static int sum(
+        int[] array, 
+        int fromWhere, 
+        int toWhere, 
+        int smallest, 
+        int largest){
         
         int sum = 0;
         
@@ -15,16 +17,21 @@ public class Program {
             fromWhere = 0;
         }
         
-        if (toWhere > array.length - 1){
-            toWhere = array.length - 1;
+        int leng = array.length - 1;
+
+        if (toWhere > leng){
+            toWhere = leng;
         }
         
         for (int i = fromWhere; i <= toWhere; i++){
-            if (array[i] >= smallest && array[i] <= largest){
-                sum += array[i];
+
+            int n = array[i];
+
+            if (n >= smallest && n <= largest){
+                sum += n;
             }
         }
-        
+
         return sum;
     }
 
@@ -39,6 +46,4 @@ public class Program {
         System.out.println(sum(numbers, -1, 999, -10, 10));
 
     }
-
-
 }
