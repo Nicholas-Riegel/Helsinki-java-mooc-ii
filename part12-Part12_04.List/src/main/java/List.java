cdf7822f97bc8d1ea1e312deefcad6abecc86bb6
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-06-20 09:57:16 -0400

    .

--- part12-Part12_04.List/src/main/java/List.java
+++ part12-Part12_04.List/src/main/java/List.java
@@ -1,3 +1,4 @@
+
 public class List<Type> {
 
     private Type[] values;
@@ -8,17 +9,7 @@ public class List<Type> {
         this.values = (Type[]) new Object[10];
         this.firstFreeIndex = 0;
     }
-
-    public void add(Type value) {
-        
-        if(this.firstFreeIndex == this.values.length) {
-            grow();
-        }
-
-        this.values[this.firstFreeIndex] = value;
-        this.firstFreeIndex++; 
-    }
-
+    
     private void grow() {
 
         int newSize = this.values.length + this.values.length / 2;
@@ -32,6 +23,62 @@ public class List<Type> {
         this.values = newValues;
     }
 
+    public void add(Type value) {
+        
+        if (this.firstFreeIndex == this.values.length){
+            grow();
+        }
+
+        this.values[this.firstFreeIndex] = value;
+        this.firstFreeIndex++; 
+    }
+
+    public int indexOfValue(Type value) {
+        
+        for (int i = 0; i < this.firstFreeIndex; i++) {
+            if (this.values[i].equals(value)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void moveToTheLeft(int fromIndex) {
+        
+        for (int i = fromIndex; i < this.firstFreeIndex - 1; i++) {
+            this.values[i] = this.values[i + 1];
+        }
+    }
+    
+    public void remove(Type value) {
+        
+        int indexOfValue = indexOfValue(value);
+        
+        if (indexOfValue < 0) {
+            return; // not found
+        }
+
+        moveToTheLeft(indexOfValue);
+        this.firstFreeIndex--;
+    }
+
+    public boolean contains(Type value) {
+        return indexOfValue(value) >= 0;
+    }
+
+    public Type value(int index) {
+        if (index < 0 || index >= this.firstFreeIndex) {
+            throw new ArrayIndexOutOfBoundsException("Index " + index + " outside of [0, " + this.firstFreeIndex + "]");
+        }
+
+        return this.values[index];
+    }
+
+    public int size() {
+        return this.firstFreeIndex;
+    }
+    
     @Override
     public String toString(){
         String string = "";
