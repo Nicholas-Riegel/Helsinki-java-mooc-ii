603fae64f7f7af6e47e4010f5b152e61844425ca
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-06-23 16:09:11 -0400

    .

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -5,7 +5,22 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
+        
+        Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true){
+            String response = scanner.nextLine();
+            if (response.isEmpty()){
+                break;
+            }
+            int number = Integer.valueOf(response);
+
+            while (number > 0){
+                System.out.println(rand.nextInt(11));
+                number--;
+            }
+        }
     }
 
 }
