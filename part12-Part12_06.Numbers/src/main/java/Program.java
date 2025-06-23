4438cd01557448c982c6ea9ef4ecc1389cca64c6
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-06-23 16:10:52 -0400

    .

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -10,10 +10,14 @@ public class Program {
         Scanner scanner = new Scanner(System.in);
         
         while (true){
+
+            System.out.println("How many random numbers should be printed?");
             String response = scanner.nextLine();
+            
             if (response.isEmpty()){
                 break;
             }
+            
             int number = Integer.valueOf(response);
 
             while (number > 0){
